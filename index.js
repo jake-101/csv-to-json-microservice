3771123c937c24da10c979fe709f783c7b005c80
jake <jake101@gmail.com>
2019-05-09 17:57:47 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const express = require('express')
+const csv = require('csvtojson/v2')
+const bodyParser = require('body-parser')
+
+const app = express()
+module.exports = app
+
+app.use(bodyParser.text({ type: 'text/plain' }))
+
+
+app.post('*', (req, res) => {
+  if (req.body == null) {
+    return res.status(400).send({ error: 'no data in the request' })
+  }
+  console.log(req)
+csv()
+.fromString(req.body)
+.then((jsonObj)=>{
+  console.log(jsonObj);
+  res.set('Content-Type', 'application/json')
+  res.status(200).send(JSON.stringify(jsonObj, null, 4))
+
+})
+
+})
+
+app.all('*', (req, res) => {
+  res.status(405).send({ error: 'only POST requests are accepted' })
+})
