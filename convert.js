dd9fb4a35a7ef4a0c2a5c4d21b350fc95321623a
jake <jake101@gmail.com>
2019-05-10 00:36:10 -0700

    error handling added to html page

--- convert.js
+++ convert.js
@@ -1,41 +1,41 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const csv = require('csvtojson/v2')
-const bodyParser = require('body-parser')
+const express = require("express");
+const csv = require("csvtojson/v2");
+const bodyParser = require("body-parser");
 
-const app = express()
-module.exports = app
+const app = express();
+module.exports = app;
 
-app.use(bodyParser.text({ type: 'text/plain' }))
+app.use(bodyParser.text({ type: "text/plain" }));
 
-
-app.post('*', (req, res) => {
+app.post("*", (req, res) => {
   if (req.body == null) {
-    return res.status(400).send({ error: 'no data in the request' })
+    return res.status(400).send({ error: "no data in the request" });
   }
-  console.log(req)
-  let arr = []
-csv()
-.fromString(req.body)
-.subscribe((json,lineNumber)=>{
-    console.log(lineNumber, 'line number')
-arr.push(json)  }
-,onError, onComplete)
+  console.log(req);
+  let arr = [];
+  csv()
+    .fromString(req.body)
+    .subscribe(
+      (json, lineNumber) => {
+        console.log(lineNumber, "line number");
+        arr.push(json);
+      },
+      onError,
+      onComplete
+    );
 
-function onComplete() {
-  res.set('Content-Type', 'application/json')
-  res.status(200).send(JSON.stringify(arr, null, 4))
-}
+  function onComplete() {
+    res.set("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(arr, null, 4));
+  }
 
-function onError() {
+  function onError() {
+    console.log("error");
+  }
+});
 
-  console.log('error')
-}
-
-
+app.all('*', (req, res) => {
+  res.status(405).send({ error: 'only POST requests are accepted' })
 })
-
-// app.all('*', (req, res) => {
-//   res.status(405).send({ error: 'only POST requests are accepted' })
-// })
